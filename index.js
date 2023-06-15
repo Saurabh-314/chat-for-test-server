08e4a99b52589c0aadc753f2a7c23ad3425b07c6
Saurabh-314 <maxxxjob314@gmail.com>
2023-06-15 15:22:20 +0530

    some changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express"
 import { Server } from 'socket.io';
 import cors from 'cors';
 
-const PORT = "8000"
+const PORT = process.env.PORT || 8000
 const app = express();
 app.use(cors());
 
